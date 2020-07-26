5d8866b3d9233c1f0067158463e5715451abe262
duynguyen <duy511998asd@gmail.com>
2020-07-26 16:03:38 +0700

    add more config gulp + packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
-const { series, src, dest , parallel,task } = require('gulp');
-const gulp = require('gulp');
+const { series, src, dest , parallel,task,watch,gulp } = require('gulp');
 const {exec} = require('child_process');
 const {eventEmmiter} = require('events');
 const fs = require('fs');
 var del = require('del');
+const sass = require('gulp-sass');
+const sync = require('browser-sync').create();
+const merge = require('merge-stream');
+const concat = require('gulp-concat');
+sass.compiler = require('node-sass');
+function browsersync(){
+    sync.init({
+        server: './dist',
+    });
+    watching();
+}
 
+function sassCompile(){
+    src('./sass/*.sass', {sourcemaps: true}).pipe(sass().on('error',sass.logError)).pipe(dest('./dist/css'));
+}
+
+// gulp.task('sass', ()=>{
+//     return gulp.src('./sass/**/*.sass', {sourcemaps: true}).pipe(sass().on('error',sass.logError)).pipe(dest('./dist/css'));
+// })
 
 function streamTask(){
     return src('*/js').pipe(dest('dist'));
@@ -24,14 +41,26 @@ function build(cb){
     cb();
 }
 
-function copy(cb){
-    src('html/aboutus.html').pipe(dest('dist/'));
+function copy(){
+    src('html/*').pipe(dest('dist/'));
+    src('css/*').pipe(dest('dist/css/'));
 }
 
-function clean(cb){
+function watching(){
+    const watcher = watch(['html/*','css/*','sass/*']);
+    watcher.on('change',(path, stats) => {
+        clean();
+        copy();
+        sassCompile();
+        sync.reload();
+    });
+}
+function clean(){
     del.sync(['dist/*']);
 }
 const firstTask = parallel(clean, build, firstTaskGulp);
 exports.build = copy;
 exports.default = clean;
 exports.duy = copy;
+exports.watch = watching;
+exports.sync = browsersync;
